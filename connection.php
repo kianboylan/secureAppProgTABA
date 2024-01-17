3fb0cf836c2892eaf280d80f189691bd4a3a7094
kianboylan <73694653+kianboylan@users.noreply.github.com>
2024-01-17 09:31:37 +0000

    Changing DB Directory

--- connection.php
+++ connection.php
@@ -3,11 +3,11 @@
 ?>
 <?php
 	//This checks to see if the database already exists. If it doesn't then the database will be created
-	if(!is_file('db/user.sqlite3')){
-		file_put_contents('db/user.sqlite3', null);
+	if(!is_file('user.sqlite3')){
+		file_put_contents('user.sqlite3', null);
 	}
 	// This creates a connection to the database
-	$conn = new PDO('sqlite:db/user.sqlite3');
+	$conn = new PDO('sqlite:user.sqlite3');
 
 	//This sets the attributes of the connection. More specifically, if an error is detect in SQLite, PDO will throw an exception and the running of the script will cease.
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
